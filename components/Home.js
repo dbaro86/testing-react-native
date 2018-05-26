53ac94c7eb42dd5b6e0d791642874895e272350b
Duany Baro Menendez <duany@generalsoftwareinc.com>
2018-05-26 17:53:43 -0400

    Adicionado estilos con el ejemplo del capitulo 5

--- components/Home.js
+++ components/Home.js
@@ -3,15 +3,7 @@ import {Text, View} from 'react-native';
 import PresentationalComponent from './PresentationalComponent';
 class Home extends Component{
     state=  {
-        myState: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, \
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
-            Ut enim ad \
-             minim veniam, quis nostrud \
-             exercitation ullamco laboris \
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in \
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui \
-            officia deserunt mollit anim id est laborum.'
+        myState: 'This is my state'
     }
 
     updateState = () => this.setState({myState: 'The state is updated'})
@@ -19,7 +11,7 @@ class Home extends Component{
     render(){
         return (
             <View>
-                <PresentationalComponent myState = {this.state.myState} updateState= {this.updateState}/>
+                <PresentationalComponent myState = {this.state.myState}/>
             </View>
         )
     }
