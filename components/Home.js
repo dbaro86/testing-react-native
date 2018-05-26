8c372792075c997cc56aaf8c8358a912e70d95a2
Duany Baro Menendez <duany@generalsoftwareinc.com>
2018-05-26 18:21:47 -0400

    Cambiando y probando diversos estilos con el ejemplo del capitulo 6

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 import PresentationalComponent from './PresentationalComponent';
 class Home extends Component{
     state=  {
@@ -10,11 +10,42 @@ class Home extends Component{
 
     render(){
         return (
-            <View>
-                <PresentationalComponent myState = {this.state.myState}/>
+            <View style = {styles.container}>
+                <View style = {styles.redbox} />
+                <View style = {styles.bluebox} />
+                <View style = {styles.blackbox} />
             </View>
+
         )
     }
 }
 
 export default Home;
+
+const styles = StyleSheet.create({
+    container : {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        backgroundColor: 'grey',
+        height: 600
+    },
+
+    redbox : {
+        width: 100,
+        height: 100,
+        backgroundColor: 'red',
+    },
+
+    bluebox : {
+        width: 100,
+        height: 100,
+        backgroundColor: 'blue',
+    },
+
+    blackbox : {
+        width: 100,
+        height: 100,
+        backgroundColor: 'black',
+    }
+})
